47eb2701884fbb7c04961f6bff3006193a8fba48
songcs <songcs@live.com>
2016-04-11 22:06:42 +0800

    更改了路径选择策略：优先考虑q值最大的

--- QDemo.m
+++ QDemo.m
@@ -1,6 +1,6 @@
 %Rew=zeros(5);Rew(5,5)=100;    % rewards
 task=struct('initialState',[1,1],'terminalState',[8,9]); % task   initialState  初始状态  terminalState 终止状态
-robot=struct('alpha',1,'gamma',1,'Qtable',zeros(100,4),'best',[],'state',[1,1]);%alpha（学习效率） gamma(折扣因子) 学习参数  best 最优路径 state 状态 Qtable存储5*5每个状态Q值
+robot=struct('alpha',0.5,'gamma',0.5,'Qtable',zeros(100,4),'best',[],'state',[1,1]);%alpha（学习效率） gamma(折扣因子) 学习参数  best 最优路径 state 状态 Qtable存储5*5每个状态Q值
 robot=Qlearning(robot,task,Rew,1000);
 
 disp('前驱状态 | 动作 | 后继状态')

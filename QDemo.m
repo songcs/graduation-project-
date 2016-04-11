88d10597f5210b26dccc8d1f894a083f565d9655
songcs <songcs@live.com>
2016-04-11 12:03:57 +0800

    可运行，但是速度并不快

--- QDemo.m
+++ QDemo.m
@@ -1,4 +1,4 @@
-%Rew=zeros(20);Rew(19,15)=100;    % rewards
+Rew=zeros(20);Rew(19,15)=100;    % rewards
 task=struct('initialState',[1,1],'terminalState',[19,15]); % task   initialState  初始状态  terminalState 终止状态
 robot=struct('alpha',1,'gamma',1,'Qtable',zeros(400,4),'best',[],'state',[1,1]);%alpha（学习效率） gamma(折扣因子) 学习参数  best 最优路径 state 状态 Qtable存储20*20每个状态Q值
 robot=Qlearning(robot,task,Rew,200);
